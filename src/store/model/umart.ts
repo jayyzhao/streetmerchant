3a67e63f4fbd6b02f8779e7a940c7d7e8ac246fb
Jonathan Zhao <2725204+jayyzhao@users.noreply.github.com>
2025-02-08 15:08:36 +1100

    update umart

--- src/store/model/umart.ts
+++ src/store/model/umart.ts
@@ -1,4 +1,4 @@
-import {Store} from './store';
+import { Store } from './store';
 
 export const Umart: Store = {
   backoffStatusCodes: [403, 429],
@@ -18,119 +18,98 @@ export const Umart: Store = {
     },
   },
   links: [
-    {
-      brand: 'asus',
-      model: 'strix',
-      series: '3080',
-      url: 'https://www.umart.com.au/Asus-ROG-Strix-GeForce-RTX-3080-10G-Graphics-Card_56894G.html',
+    { 
+      brand: 'gigabyte', 
+      model: 'aorus xtreme waterforce', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/gigabyte-aorus-geforce-rtx-5090-xtreme-waterforce-32g-graphics-card-gv-n5090aorusx-w-32gd-81385' 
     },
-    {
-      brand: 'asus',
-      model: 'strix oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/Asus-ROG-Strix-GeForce-RTX-3080-OC-10G-Graphics-Card_56893G.html',
+    { 
+      brand: 'gigabyte', 
+      model: 'aorus xtreme waterforce', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/gigabyte-aorus-geforce-rtx-5090-xtreme-waterforce-wb-32g-graphics-card-gv-n5090aorusx-wb-32gd-81386' 
     },
-    {
-      brand: 'asus',
-      model: 'tuf',
-      series: '3080',
-      url: 'https://www.umart.com.au/Asus-GeForce-RTX-3080-TUF-Gaming-10G-Graphics-Card_56792G.html',
+    { 
+      brand: 'gigabyte', 
+      model: 'master ice', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/gigabyte-aorus-geforce-rtx-5090-master-ice-32g-graphics-card-gv-n5090aorusm-ice-32gd-81387'
     },
-    {
-      brand: 'asus',
-      model: 'tuf oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/Asus-GeForce-RTX-3080-TUF-Gaming-OC-10G-Graphics-Card_56895G.html',
+    { 
+      brand: 'gigabyte', 
+      model: 'aorus master', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/gigabyte-aorus-geforce-rtx-5090-master-32g-graphics-card-gv-n5090aorus-m-32gd-81388' 
     },
-    {
-      brand: 'evga',
-      model: 'ftw3 ultra',
-      series: '3080',
-      url: 'https://www.umart.com.au/EVGA-GeForce-RTX-3080-FTW3-Ultra-Gaming-10G-Graphics-Card_57050G.html',
+    { 
+      brand: 'gigabyte', 
+      model: 'gaming oc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/gigabyte-geforce-rtx-5090-gaming-oc-32g-graphics-card-gv-n5090gaming-oc-32gd-81389' 
     },
-    {
-      brand: 'evga',
-      model: 'xc3 ultra',
-      series: '3080',
-      url: 'https://www.umart.com.au/EVGA-GeForce-RTX-3080-XC3-Ultra-Gaming-10G-Graphics-Card_57049G.html',
+    { 
+      brand: 'gigabyte', 
+      model: 'windforce', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/gigabyte-geforce-rtx-5090-windforce-oc-32g-graphics-card-gv-n5090wf3oc-32gd-81390' 
     },
-    {
-      brand: 'galax',
-      model: 'sg oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/Galax-GeForce-RTX-3080-SG--1-Click-OC--10G-Graphics-Card_56817G.html',
+    { 
+      brand: 'msi', 
+      model: 'suprim liquid soc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/msi-geforce-rtx-5090-32g-suprim-liquid-soc-graphics-card-geforce-rtx-5090-32g-suprim-liquid-soc-81402' 
     },
-    {
-      brand: 'gigabyte',
-      model: 'aorus master',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-Aorus-GeForce-RTX-3080-Master-10G-Graphics-Card_56885G.html',
+    { 
+      brand: 'msi', 
+      model: 'suprim soc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/msi-geforce-rtx-5090-32g-suprim-soc-graphics-card-geforce-rtx-5090-32g-suprim-soc-81403'
     },
-    {
-      brand: 'gigabyte',
-      model: 'aorus xtreme',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-AORUS-GeForce-RTX-3080-Xtreme-10G-Graphics-Card_56969G.html',
+    { 
+      brand: 'msi', 
+      model: 'vanguard soc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/msi-geforce-rtx-5090-32g-vanguard-soc-launch-edition-graphics-card-geforce-rtx-5090-32g-vanguard-soc-launch-edition-81404'
     },
-    {
-      brand: 'gigabyte',
-      model: 'aorus xtreme waterforce',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-Aorus-GeForce-RTX-3080-Xtreme-Waterforce-10G-Graphics-Card_57650G.html',
+    { 
+      brand: 'msi', 
+      model: 'vanguard soc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/msi-geforce-rtx-5090-32g-vanguard-soc-graphics-card-geforce-rtx-5090-32g-vanguard-soc-81405' 
     },
-    {
-      brand: 'gigabyte',
-      model: 'aorus xtreme waterforce wb',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-GeForce-RTX-3080-Xtreme-WaterForce-WB-10G-Graphics-Card_57486G.html',
+    { 
+      brand: 'msi', 
+      model: 'gaming trio oc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/msi-geforce-rtx-5090-32g-gaming-trio-oc-graphics-card-geforce-rtx-5090-32g-gaming-trio-oc-81406'
     },
-    {
-      brand: 'gigabyte',
-      model: 'eagle',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-GeForce-RTX-3080-Eagle-10G-Graphics-Card_57335G.html',
+    { 
+      brand: 'msi', 
+      model: 'ventus 3x oc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/msi-geforce-rtx-5090-32g-ventus-3x-oc-graphics-card-geforce-rtx-5090-32g-ventus-3x-oc-81407'
     },
-    {
-      brand: 'gigabyte',
-      model: 'eagle oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-GeForce-RTX-3080-Eagle-OC-10G-Graphics-Card_56791G.html',
+    { 
+      brand: 'asus', 
+      model: 'rog astral', series: '5090', url: 'https://umart.com.au/product/asus-rog-astral-lc-geforce-rtx-5090-oc-32g-gaming-graphics-card-rog-astral-lc-rtx5090-o32g-gaming-81414' },
+    { 
+      brand: 'asus', 
+      model: 'astral oc', 
+      series: '5090',
+      url: 'https://umart.com.au/product/asus-rog-astral-geforce-rtx-5090-oc-32g-gaming-graphics-card-rog-astral-rtx5090-o32g-gaming-81415'
     },
-    {
-      brand: 'gigabyte',
-      model: 'gaming oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-GeForce-RTX-3080-Gaming-OC-10G-Graphics-Card_56790G.html',
+    { 
+      brand: 'asus', 
+      model: 'tuf oc', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/asus-tuf-geforce-rtx-5090-oc-32g-gaming-graphics-card-tuf-rtx5090-o32g-gaming-81416'
     },
-    {
-      brand: 'gigabyte',
-      model: 'vision oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/Gigabyte-GeForce-RTX-3080-Vision-10G-OC-Graphics-Card_56886G.html',
-    },
-    {
-      brand: 'inno3d',
-      model: 'ichill x4',
-      series: '3080',
-      url: 'https://www.umart.com.au/Inno3D-GeForce-RTX-3080-iCHILL-X4-10G-Graphics-Card_56962G.html',
-    },
-    {
-      brand: 'msi',
-      model: 'gaming x trio',
-      series: '3080',
-      url: 'https://www.umart.com.au/MSI-GeForce-RTX-3080-Gaming-X-Trio-10G-Graphics-Card_56788G.html',
-    },
-    {
-      brand: 'msi',
-      model: 'suprim x',
-      series: '3080',
-      url: 'https://www.umart.com.au/MSI-GeForce-RTX-3080-Suprim-X-10G-Graphics-Card_57617G.html',
-    },
-    {
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
-      url: 'https://www.umart.com.au/MSI-GeForce-RTX-3080-Ventus-3X-10G-Graphics-Card_56789G.html',
+    { 
+      brand: 'asus', 
+      model: 'tuf gaming', 
+      series: '5090', 
+      url: 'https://umart.com.au/product/asus-tuf-geforce-rtx-5090-32g-gaming-graphics-card-tuf-rtx5090-32g-gaming-81417' 
     },
   ],
   name: 'umart',
